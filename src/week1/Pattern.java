fd560db840e7c2fb6bb0a81549cb3401a78f275f
Suhasa <Suhasa@Suhasa-PC>
2019-06-01 22:46:21 +0530

    task3 in progress

--- src/week1/Pattern.java
+++ src/week1/Pattern.java
@@ -1,6 +1,12 @@
 package week1;
 
 public class Pattern {
-	
-
+		public static void main(String args[]) {
+		for (int i=66; i <= 84; i=i+2) {
+			for (int j = 65; j < i; j=j+2) {
+				System.out.print((char)j);
+			}
+			System.out.println();
+		}
+	}
 }
