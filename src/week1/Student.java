c1598b5a7bd10e072cbe85b184f5d1a8cee78cc7
Suhasa <Suhasa@Suhasa-PC>
2019-06-01 21:44:15 +0530

    Task2 done

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -6,9 +6,13 @@ public class Student {
 	int std;
 	float marks;
 	public Student(int i, String string, int j, float d) {
-	 id=i;
-	 name=string;
-	 j=std;
-	 marks=d;
+		id=i;
+		name=string;
+		j=std;
+		marks=d;
+	}	
+public static void main (String args[]) {
+		Student student1 = new Student(1, "Suhasa", 12, 80);
+		System.out.println("Student ID = "+student1.id+" "+"Student name = "+student1.name+" "+"Standard = "+student1.std+" "+"Marks Scored = "+student1.marks);
 	}
 }
