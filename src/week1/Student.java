3bca5573b493fe844305f747835042f8b7dde20c
Suhasa <Suhasa@Suhasa-PC>
2019-06-01 22:54:42 +0530

    task1 done

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -11,7 +11,7 @@ public class Student {
 		j=std;
 		marks=d;
 	}	
-public static void main (String args[]) {
+	public static void main (String args[]) {
 		Student student1 = new Student(1, "Suhasa", 12, 80);
 		System.out.println("Student ID = "+student1.id+" "+"Student name = "+student1.name+" "+"Standard = "+student1.std+" "+"Marks Scored = "+student1.marks);
 	}
