24912789b582f9c18437a6761c13ccd1fa0031ed
Suhasa <Suhasa@Suhasa-PC>
2019-06-01 21:35:34 +0530

    task1 done

---
+++ src/week1/Student.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Student {
+	int id;
+	String name;
+	int std;
+	float marks;
+	public Student(int i, String string, int j, float d) {
+	 id=i;
+	 name=string;
+	 j=std;
+	 marks=d;
+	}
+}
--- src/week1/Task1.java
+++ src/week1/Task1.java
@@ -1,6 +1,14 @@
 package week1;
+import week1.Student;
 
+/**
+ * @author Suhasa
+ *
+ */
 public class Task1 {
-
 	
+	public static void main (String args[]) {
+		Student student1 = new Student(1, "Suhasa", 12, 80);
+		System.out.println("Student ID = "+student1.id+" "+"Student name = "+student1.name+" "+"Standard = "+student1.std+" "+"Marks Scored = "+student1.marks);
+	}
 }
